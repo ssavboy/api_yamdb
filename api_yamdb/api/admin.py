1d625afde58b1afdfba15675906f52a71b7d1c15
Мария Ковалева <shedroom@gmail.com>
2022-09-18 18:30:59 +0300

    text

--- api_yamdb/api/admin.py
+++ api_yamdb/api/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from reviews.models import Category, Genre, Title
+# from reviews.models import Category, Genre, Title
 
 # TODO: Нужны ли эти импорты?
 # admin.site.register(Category)
