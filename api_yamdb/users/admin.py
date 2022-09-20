de97efcbe9f48d9f056a4115a311e4948c29a1bb
Kirill Molchanov <molchanov.web.dev@yandex.ru>
2022-09-20 20:56:33 +0300

    fix users/rdme

--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -2,4 +2,16 @@ from django.contrib import admin
 
 from .models import User
 
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'username',
+        'first_name',
+        'last_name',
+        'email',
+        'role',
+        'bio',
+    )
+    search_fields = ('username', 'role',)
+    list_filter = ('username',)
