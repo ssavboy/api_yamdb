5ca5e9fd358d378683c60203557447a230ef7418
Мария Ковалева <shedroom@gmail.com>
2022-09-21 11:31:57 +0300

    мои замечания кончились

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import permissions
 from django.db.models import Avg
 from rest_framework import viewsets, filters
 from django_filters.rest_framework import DjangoFilterBackend
