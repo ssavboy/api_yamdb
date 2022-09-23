178ba02c06ab402c1df3d8d99bf1b9ecbf75f204
Kirill Molchanov <molchanov.web.dev@yandex.ru>
2022-09-23 17:46:19 +0300

    final

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,7 +1,7 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-from django.conf import settings
 from .mixins import UsernameValidatorMixin
 
 USER = 'user'
