0de9d004509c7bf219ae86efc7dccef139f220f8
Kirill Molchanov <molchanov.web.dev@yandex.ru>
2022-09-23 17:55:25 +0300

    review#3

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -40,21 +40,3 @@ admin.site.register(Comment)
 admin.site.register(Genre)
 admin.site.register(Title)
 admin.site.register(Review)
-
-class CommentAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'review', 'text', 'author', 'pub_date',)
-    search_fields = ('review', 'text', 'author', 'pub_date',)
-    list_filter = ('review', 'text', 'author', 'pub_date',)
-
-
-class ReviewAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'title', 'text', 'score', 'author', 'pub_date',)
-    search_fields = ('title', 'text', 'author', 'pub_date',)
-    list_filter = ('title', 'text', 'score', 'author', 'pub_date',)
-
-
-admin.site.register(Category)
-admin.site.register(Comment)
-admin.site.register(Genre)
-admin.site.register(Title)
-admin.site.register(Review)
